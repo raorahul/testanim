e75eb453637af4725132d16035bb51cefd455727
Rahul Rao <rao.rahul@hotmail.com>
2015-05-02 17:54:35 -0400

    Setting up base container.

--- js/app/canvas-type.js
+++ js/app/canvas-type.js
@@ -1,7 +1,7 @@
 /*jshint unused:vars*/
 /*jshint undef:true*/
 /*global
-define
+define, createjs, document, window
 */
 
 (function (){
@@ -10,7 +10,84 @@ define
   define(function () {
 
     // Add canvas type code here.
+    var yieldmoItems = {};
+    var proto;
 
+    function CanvasDemo (containerId) {
+
+      /*
+                PRIVATE VARIABLES
+       */
+
+      this._pixelRatio = window.devicePixelRatio || 1;
+      this._screenX = 0;
+      this._screenY = 0;
+      this._screenWidth = 750 * this._pixelRatio;
+      this._screenHeight = 1334 * this._pixelRatio;
+
+      this._stageWidth = 0;
+      this._stageHeight = 0;
+
+      this._canvasNode = null;
+      this._containerNode = null;
+
+      this._stage = null;
+
+
+      /*
+        SETUP
+       */
+      this._setup(containerId);
+    }
+
+    proto = CanvasDemo.prototype;
+    proto._setup = function (containerId) {
+
+      // Add the canvas DOM element.
+      this._containerNode = document.getElementById(containerId);
+      if (!this._containerNode) {
+        return;
+      }
+
+      this._stageWidth = this._containerNode.clientWidth * this._pixelRatio;
+      this._stageHeight = this._screenHeight + (200 * this._pixelRatio); // random 200 px padding.
+
+      this._containerNode.innerHTML = '<canvas id="canvas-type" width=" ' + this._stageWidth + '" height="' + this._stageHeight + '"></canvas>';
+      this._canvasNode = document.getElementById('canvas-type');
+
+      this._canvasNode.style.width =  (this._stageWidth / this._pixelRatio) + 'px';
+      this._canvasNode.style.height =  (this._stageHeight / this._pixelRatio) + 'px';
+
+
+      // Calculate the screen dimensions and placement.
+      this._screenX = Math.min(this._screenWidth, ((this._stageWidth - this._screenWidth) / 2.0));
+      this._screenY = Math.min(this._screenHeight, ((this._stageHeight - this._screenHeight) / 2.0));
+
+      // Setup the stage.
+      this._stage = new createjs.Stage(this._canvasNode);
+      createjs.Touch.enable(this._stage);
+      this._stage.enableMouseOver(20);
+
+      // Draw the wrapper container.
+      this._setupWrapper();
+
+      this._stage.update();
+    };
+
+    proto._setupWrapper = function () {
+      var box = new createjs.Shape();
+      box.graphics
+      .ss(2)
+      .s('#404040')
+      .r(0, 0, this._screenWidth, this._screenHeight);
+      box.x = this._screenX;
+      box.y = this._screenY;
+
+      this._stage.addChild(box);
+    };
+
+    yieldmoItems.CanvasDemo = CanvasDemo;
+    return yieldmoItems;
 
   });
 })();
--- js/app/main.js
+++ js/app/main.js
@@ -9,9 +9,10 @@ require
   require.config({
     baseUrl: 'js/app'
     // paths: {
-    //   'jquery': 'http://code.jquery.com/jquery-1.11.2.min',
-    //   'manipulatives': './manipulatives',
-    //   'instructions': './instructions',
+    //   // 'createjs': 'https://code.createjs.com/createjs-2014.12.12.min'
+    //   // 'jquery': 'http://code.jquery.com/jquery-1.11.2.min',
+    //   // 'manipulatives': './manipulatives',
+    //   // 'instructions': './instructions',
     // },
     // shim: {
     //   'createjs': {
@@ -23,6 +24,9 @@ require
 
   // Start the main app logic.
   require(['canvas-type'], function (canvasType)  {
+
+    // Setup the canvas.
+    new canvasType.CanvasDemo('container');
 
     // // Setup the question configuration
     // var config = {};
